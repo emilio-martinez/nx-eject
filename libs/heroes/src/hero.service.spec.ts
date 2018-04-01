cb461250ea430695562152b66d8fde3dd0512a0f
Emilio Martinez <e.marticor@gmail.com>
2018-04-01 02:12:17 -0700

    test(heroes): test for `getHeroes`

--- libs/heroes/src/hero.service.spec.ts
+++ libs/heroes/src/hero.service.spec.ts
@@ -7,6 +7,7 @@ import { MessageService } from '@myprojectname/messages';
 import { Hero } from './hero';
 
 describe('HeroService', () => {
+  const apiUrl = 'api/heroes';
   let heroService: HeroService;
   let messageService: MessageService;
   let httpMock: HttpTestingController;
@@ -33,12 +34,36 @@ describe('HeroService', () => {
     httpMock.verify();
   });
 
-  it('should be created', () => {
-    expect(heroService).toBeTruthy();
+  describe('getHeroes', () => {
+    it('should return observable with hero array', () => {
+      const mockUsers: Hero[] = [{ id: 0, name: 'Mr. Incredible' }, { id: 1, name: 'Mr. Potato Head' }];
+
+      heroService.getHeroes().subscribe(res => {
+        expect(res.length).toBe(2);
+        expect(messageService.messages.length).toBe(1);
+        expect(messageService.messages[0].includes('fetched heroes')).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(apiUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockUsers);
+    });
+
+    it('should return empty when requests errors', () => {
+      heroService.getHeroes().subscribe(res => {
+        expect(res.length).toBe(0);
+        expect(messageService.messages.length).toBe(1);
+        expect(messageService.messages[0].includes('getHeroes failed')).toBeTruthy();
+      });
+
+      const req = httpMock.expectOne(apiUrl);
+      expect(req.request.method).toBe('GET');
+      req.flush(null, { status: 400, statusText: 'Error' });
+    });
   });
 
   describe('searchHeroes', () => {
-    const endpoint = (term: string) => `api/heroes/?name=${term}`;
+    const endpoint = (term: string) => `${apiUrl}/?name=${term}`;
 
     it('should return empty when no term is provided', () => {
       heroService.searchHeroes('').subscribe(res => {
--- libs/heroes/src/hero.service.ts
+++ libs/heroes/src/hero.service.ts
@@ -53,7 +53,7 @@ export class HeroService {
       return of([]);
     }
     return this.http
-      .get<Hero[]>(`api/heroes/?name=${term}`)
+      .get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
       .pipe(
         tap(_ => this.log(`found heroes matching "${term}"`)),
         catchError(this.handleError<Hero[]>('searchHeroes', []))
