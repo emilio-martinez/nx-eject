d4010d27f4b29ae032c4eb5a00a689f274222322
Emilio Martinez <e.marticor@gmail.com>
2018-04-03 11:20:28 -0700

    test(heroes): scaffold heroes dahsboard test

--- libs/heroes/src/hero-dashboard/hero-dashboard.component.spec.ts
+++ libs/heroes/src/hero-dashboard/hero-dashboard.component.spec.ts
@@ -1,26 +1,84 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { of as observableOf } from 'rxjs/observable/of';
 
 import { HeroDashboardComponent } from './hero-dashboard.component';
+import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+class MockHeroService implements Partial<HeroService> {
+  private heroes: Hero[] = [{ id: 0, name: 'Mr. Incredible' }, { id: 1, name: 'Mr. Potato Head' }];
+
+  getHeroes(): Observable<Hero[]> {
+    return observableOf(this.heroes.slice());
+  }
+}
 
 describe('DashboardComponent', () => {
-  let component: HeroDashboardComponent;
-  let fixture: ComponentFixture<HeroDashboardComponent>;
+  describe('isolated', () => {
+    let component: HeroDashboardComponent;
+    let service: MockHeroService;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [HeroDashboardComponent]
-      }).compileComponents();
-    })
-  );
+    beforeEach(() => {
+      service = new MockHeroService();
+      component = new HeroDashboardComponent(service as any);
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeroDashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    it('should be created', () => {
+      expect(component).toBeTruthy();
+    });
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  describe('shallow', () => {
+    let component: HeroDashboardComponent;
+    let fixture: ComponentFixture<HeroDashboardComponent>;
+
+    beforeEach(
+      async(() => {
+        TestBed.configureTestingModule({
+          declarations: [HeroDashboardComponent],
+          providers: [{ provide: HeroService, useClass: MockHeroService }],
+          schemas: [NO_ERRORS_SCHEMA]
+        }).compileComponents();
+      })
+    );
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HeroDashboardComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should be created', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('integration', () => {
+    let component: HeroDashboardComponent;
+    let fixture: ComponentFixture<HeroDashboardComponent>;
+
+    beforeEach(
+      async(() => {
+        TestBed.configureTestingModule({
+          imports: [RouterTestingModule],
+          declarations: [HeroDashboardComponent, HeroSearchComponent],
+          providers: [{ provide: HeroService, useClass: MockHeroService }]
+        }).compileComponents();
+      })
+    );
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HeroDashboardComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should be created', () => {
+      expect(component).toBeTruthy();
+    });
   });
 });
